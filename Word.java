84dbfa0dd60eb9028cd4c3bac16d39030a116b71
TaeHyungK <xogud3125@naver.com>
2017-04-10 13:06:10 +0900

    WordTestApp

---
+++ Word.java
@@ -0,0 +1,15 @@
+package com.example.test.taehyung.wordtestapp;
+
+/**
+ * Created by taehyung on 2017-01-17.
+ */
+
+public class Word {
+    String eng;
+    String kor;
+
+    Word(String eng, String kor){
+        this.eng = eng;
+        this.kor = kor;
+    }
+}
